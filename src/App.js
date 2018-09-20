f5a29e6b179ff9eabbafe545c2405163861d8770
DianaApz <kari_dia_12@hotmail.com>
2018-09-19 23:45:42 -0500

    g-f

--- src/App.js
+++ src/App.js
@@ -1,83 +1,99 @@
 import React, { Component } from 'react';
 import firebase from 'firebase';
-import logo from './logo.svg';
-import Upload from './fileImg.js'
 import './App.css';
 
 
 class App extends Component {
 
-  constructor (){
+  constructor() {
     super();
     this.state = {
-      user : null
+      user: null
     };
-    this.Google= this.Google.bind(this);
+    this.Google = this.Google.bind(this);
+    this.Facebook = this.Facebook.bind(this);
     this.logOut = this.logOut.bind(this);
-    this.LogGoogle = this.LogGoogle.bind(this);
+    this.LoginGeneral = this.LoginGeneral.bind(this);
+    this.writeUserData=this.writeUserData.bind(this);
   }
 
-  componentWillMount (){
-    firebase.auth().onAuthStateChanged(user=>{
-      this.setState({user})
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ user });
+      
     })
   }
-
-Google(){
-   const provider = new firebase.auth.GoogleAuthProvider();
-
-   firebase.auth().signInWithPopup(provider)
-   .then(response=>console.log(`${response.user.email} ha iniciado sesión`))
-   .catch((err=>console.log("${err.code} erro")))
-}
-
-Facebook(){
-  const provider = new firebase.auth.FacebookAuthProvider();
-  firebase.auth().signInWithPopup(provider)
-  .then(result=>{
-    const token = result.credential.accessToken;
-    const user = result.user;
-    console.log(`${user} hola`)})
-  .catch((err=>console.log("${err.code} erro")))
-}
-
-logOut(){
-  firebase.auth().signOut()
-  .then(result=>{})
-  .catch((err=>console.log("${err.code} erro")))
-}
-
-LogGoogle(){
-  if(this.state.user){
-    return (
-    <div>
-    <Upload/>
-      <p> hola {this.state.user.displayName}</p>
-      <img width="30%" src={this.state.user.photoURL} />   
-      <br/>
-      <button onClick={this.logOut}>Cierra Sesión</button>
-    </div>)
-  }else {
-    return(
-      <div>
-    <button onClick={this.Google}>Inicia Sesión con Google</button>
-    <button onClick={this.Facebook}>Inicia Sesión con Facebook</button>
-    </div>
-  )
+  writeUserData(userId, email, firstName,img) {
+    firebase.database().ref('users/' + userId + '/').set({
+      username: name,
+      email: email,
+      profile_picture : img,
+    });
   }
-}
+
+  Google() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then((res) => {
+        this.Upload(res.user.uid,res.user.email,res.user.displayName,res.user.photoURL)
+      })
+      .catch((err => console.log(`${err.code} erro`)
+      ));
+  }
+
+  Facebook() {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => {
+        // const token = result.credential.accessToken;
+        this.writeUserData(res.user.uid,res.user.email,res.user.displayName,res.user.photoURL)
+      })
+      .catch((err => console.log(`${err.code} erro`)))
+  }
+
+  logOut() {
+    firebase.auth().signOut()
+      .then(result => { })
+      .catch((err => console.log(`${err.code} erro`)))
+  }
+
+  LoginGeneral() {
+    if (this.state.user) {
+      this.writeUserData(res.state.user.uid,res.state.user.email,res.state.user.displayName,res.state.user.photoURL)
+      return (
+        <div>
+          <img width="100" src={this.state.user.photoURL} alt={this.state.user.photoURL} />
+          <p> hola {this.state.user.displayName}</p>
+          <button onClick={this.logOut}>Cierra Sesión</button>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.Google}>Inicia Sesión con Google</button>
+          <button onClick={this.Facebook}>Inicia Sesión con Facebook</button>
+        </div>
+      );
+    }
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to red social</h1>
-          {this.LogGoogle()}
-             </header>
+        </header>
+        <p className="App-intro">
+          {this.LoginGeneral()}
+        </p>
+
+
+
       </div>
     );
-  }}
+  }
+}
 
 
 export default App;
